a540ce99e669592a481cd5f2063688d0ac004b08
Binh Ngo <bnngo97@gmail.com>
2023-02-06 10:40:06 -0800

    created all necessary models according to the given instructions

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  foreignKey: "product_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product, {
+  foreingKey: "tag_id",
+});
 module.exports = {
   Product,
   Category,
